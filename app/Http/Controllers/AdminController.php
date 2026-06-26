a0a7659771facd43576aa32580338c413d136f5d
xhaiwa <clement.jouenne.pro@gmail.com>
2026-06-26 11:47:05 +0200

    feat: GET /api/admin/users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,12 @@ use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AdminController extends Controller {
 
+    public function users()
+    {
+        $utilisateurs = Utilisateurs::select(
+            'id_utilisateur', 'nom', 'prenom', 'email', 'admin', 'validation'
+        )->get();
+
+        return response()->json($utilisateurs);
+    }
 }
