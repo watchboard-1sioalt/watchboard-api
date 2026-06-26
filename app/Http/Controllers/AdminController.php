05701829547b476e1de3c082f893a6c878758bd7
xhaiwa <clement.jouenne.pro@gmail.com>
2026-06-26 11:42:36 +0200

    feat: starting AdminController

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Utilisateurs;
+
+use Illuminate\Http\Request;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AdminController extends Controller {
+
+}
