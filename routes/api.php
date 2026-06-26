07a76fec368d350424e2ec74b032dc990de539ef
xhaiwa <clement.jouenne.pro@gmail.com>
2026-06-26 11:54:58 +0200

    feat: PUT /api/admin/users/{id}/validate

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,7 @@ Route::middleware('auth:api')->prefix('feeds')->controller(FeedController::class
 Route::middleware('auth:api')->prefix('admin')->controller(AdminController::class)->group(function () {
     Route::get('users', 'users');
     Route::get('users/{id}', 'user');
+    Route::put('users/{id}/validate', 'validate');
 });
 
 Route::middleware('auth:api')->prefix('ressources')->controller(RessourceController::class)->group(function () {
