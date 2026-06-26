69533c65945670da15aba4993dfadc2fb897b65f
Twixty <twixty.contact@gmail.com>
2026-06-26 09:10:22 +0200

    fix vendor packages bug

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit36793882908b6cbdd344b5a4840231db::getLoader();
+return ComposerAutoloaderInit7e011e5678774712a244ff3cc4e05454::getLoader();
