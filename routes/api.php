1856ada561ae1661f340c5d860f709d5ec10cf55
xhaiwa <clement.jouenne.pro@gmail.com>
2026-06-29 16:13:32 +0200

    feat: fix token refresh route /auth/refresh

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
+    Route::post('refresh', 'refresh');
 
     Route::middleware('auth:api')->group(function () {
         Route::get('me', 'me');
         Route::put('me', 'updateMe');
         Route::put('password', 'updatePassword');
-        Route::post('refresh', 'refresh');
         Route::post('logout', 'logout');
     });
 });
